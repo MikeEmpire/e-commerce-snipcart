543dfc702c9afb830010603a6985f4bb3ff5c312
Afam Olie <mikeolie@mikesImac.domain.com>
2020-05-06 12:25:29 -0700

    added .env and skeleton for website

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  console.log(process.env)
+  return (
+    <div className="App">
+      Content will live here!
+    </div>
+  );
+}
+
+export default App;
