b4b2365f81e6558beb823ecd958d8caeecfbef66
Afam Olie <mikeolie@mikesImac.domain.com>
2020-05-06 15:25:23 -0700

    interim commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 import Navbar from "./containers/Navbar";
-import MobileNav from './containers/MobileNav'
+import MobileNav from './containers/MobileNav';
+
+import Splash from './presentation/Splash';
 
 const App = () => (
   <div className="App">
     <MobileNav />
     <Navbar />
+    <div className="site">
+      <Splash />
+    </div>
   </div>
 );
 
---
+++ src/components/presentation/Splash.js
@@ -0,0 +1,34 @@
+import React from "react";
+const splashImage = require("../../images/intro-image-01.jpg");
+
+const Splash = () => (
+  <div id="intro" className="intro intro-01 intro-fullscreen clearfix">
+    <div
+      className="intro-image parallax blur-fx trans-20"
+      style={{ backgroundImage: `url(${splashImage})` }}
+    >
+      <img className="hidden" src={splashImage} alt="Splash screen" />
+    </div>
+
+    <div className="text-slider" data-delay="5">
+      <div className="text-slide visible">
+        <h2 className="text-fx">Custom Design</h2>
+        <h6 className="text-fx-word">
+          browse our selection of high detailed customized vehicles
+        </h6>
+      </div>
+      <div className="text-slide">
+        <h2 className="text-fx">Music Player</h2>
+        <h6 className="text-fx-word">
+          is the industry standard music player for a fully responsive design
+          that scales automatically to suit any device
+        </h6>
+      </div>
+    </div>
+    <span className="scroll-discover">
+      <span className="icon icon-line-arrow-left"></span> Scroll to discover
+    </span>
+  </div>
+);
+
+export default Splash;
